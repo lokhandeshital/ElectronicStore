eed3bad62770759643bfecb5ee956edc7b6ddb24
Shital lokhande <shitallokhande596@gmail.com>
2023-06-06 13:20:54 +0530

    Project Code upto Log File

--- src/main/java/com/bikkadit/electronic/store/ElectronicStoreApplication.java
+++ src/main/java/com/bikkadit/electronic/store/ElectronicStoreApplication.java
@@ -9,7 +9,7 @@ public class ElectronicStoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(ElectronicStoreApplication.class, args);
 
-        System.out.println("Electronic Store Application Running.........");
+        System.out.println("Electronic Store Application Running........");
 
     }
 
