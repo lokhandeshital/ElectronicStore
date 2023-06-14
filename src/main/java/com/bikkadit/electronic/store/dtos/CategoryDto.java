313b7216544b95ab82f20fe2c5c7a391778bb952
Shital lokhande <shitallokhande596@gmail.com>
2023-06-14 13:42:42 +0530

    Project Code Changes upto CategoryDto class

---
+++ src/main/java/com/bikkadit/electronic/store/dtos/CategoryDto.java
@@ -0,0 +1,31 @@
+package com.bikkadit.electronic.store.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDto {
+
+    private String categoryId;
+
+    @Size(min = 4, message = "Title Contain Min 4 Character !!")
+    @NotEmpty
+    private String title;
+
+    @NotBlank(message = "Description Required !!")
+    private String description;
+
+    private String coverImage;
+
+
+}
--- src/main/java/com/bikkadit/electronic/store/dtos/CustomDto.java
+++ src/main/java/com/bikkadit/electronic/store/dtos/CustomDto.java
@@ -4,6 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -11,10 +17,13 @@ import lombok.Setter;
 @AllArgsConstructor
 public class CustomDto {
 
-
     private String isActive;
 
+    @NotEmpty
+    @Size(min = 3,max = 20,message = "Creater Name Should be Min 3 and Max 20 Character")
     private String createdBy;
 
+    @NotEmpty
+    @Size(min = 2,max = 15,message = "Modifier Name Should be Min 2 and Max 15 Character ")
     private String modifiedBy;
 }
