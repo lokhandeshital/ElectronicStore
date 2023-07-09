412def58c2e4dc46437afe18ec509183126c6349
Shital lokhande <shitallokhande596@gmail.com>
2023-07-09 12:57:03 +0530

    Project Code Changes upto Product Controller Test for Create Product Test

--- src/main/java/com/bikkadit/electronic/store/dtos/ProductDto.java
+++ src/main/java/com/bikkadit/electronic/store/dtos/ProductDto.java
@@ -12,29 +12,22 @@ import javax.validation.constraints.Size;
 @AllArgsConstructor
 @ToString
 @Builder
-public class ProductDto extends CustomDto{
+public class ProductDto extends CustomDto {
 
     private String productId;
 
-    @NotEmpty
-    @Size(min = 2,max = 50,message = "Title Contain min 2 and max 50 Character !!")
     private String title;
 
-    @NotEmpty
     private String description;
 
-    @NotEmpty
     private Double price;
 
     private Double discountedPrice;
 
-    @NotEmpty
     private Long quantity;
 
-    @NotEmpty
     private Boolean live;
 
-    @NotEmpty
     private Boolean stock;
 
 
---
+++ src/test/java/com/bikkadit/electronic/store/controller/ProductControllerTest.java
@@ -0,0 +1,86 @@
+package com.bikkadit.electronic.store.controller;
+
+import com.bikkadit.electronic.store.dtos.ProductDto;
+import com.bikkadit.electronic.store.model.Product;
+import com.bikkadit.electronic.store.service.ProductService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductControllerTest {
+
+    @MockBean
+    private ProductService productService;
+
+    private Product product;
+
+    @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void init() {
+
+        product = Product.builder().title("Samsung Tv")
+                .description("This Tv Contain many Feature")
+                .quantity(10l)
+                .price(60000.00)
+                .discountedPrice(55000.00)
+                .stock(true)
+                .live(true)
+                .build();
+    }
+
+    // Create Category Test
+    @Test
+    public void createCategoryTest() throws Exception {
+
+        ProductDto productDto = mapper.map(product, ProductDto.class);
+        Mockito.when(productService.createProduct(Mockito.any())).thenReturn(productDto);
+
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders.post("/api/products/")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(converObjectToJsonString(product))
+                                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.title").exists());
+
+    }
+
+    private String converObjectToJsonString(Object product) {
+
+        try {
+            return new ObjectMapper().writeValueAsString(product);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // Update Category Test
+
+    // Delete Category Test
+
+    // Get All Category Test
+
+
+}
