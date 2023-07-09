e501e789f59d86c67cb34718ff7f07d57cfa9655
Shital lokhande <shitallokhande596@gmail.com>
2023-07-09 12:39:24 +0530

    Project Code Changes upto Category Controller Test for Get All Category Test

--- src/test/java/com/bikkadit/electronic/store/controller/CategoryControllerTest.java
+++ src/test/java/com/bikkadit/electronic/store/controller/CategoryControllerTest.java
@@ -1,6 +1,7 @@
 package com.bikkadit.electronic.store.controller;
 
 import com.bikkadit.electronic.store.dtos.CategoryDto;
+import com.bikkadit.electronic.store.dtos.PageableResponse;
 import com.bikkadit.electronic.store.model.Category;
 import com.bikkadit.electronic.store.service.CategoryService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,6 +16,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Arrays;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -107,6 +110,40 @@ public class CategoryControllerTest {
                                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk());
+
+    }
+
+    // Get All Category
+    @Test
+    public void getAllCategoryTest() throws Exception {
+
+        CategoryDto categoryDto1 = CategoryDto.builder().title("Mobile Phone")
+                .description("This Category Contain Diff Type of Mobile Phone")
+                .coverImage("Phone.png")
+                .build();
+
+        CategoryDto categoryDto2 = CategoryDto.builder().title("Mixer")
+                .description("This Category Contain Diff Type of Mixer")
+                .coverImage("Mixer.png")
+                .build();
+
+        CategoryDto categoryDto3 = CategoryDto.builder().title("Samsung Tv")
+                .description("This Category Contain Diff Type of Samsung Tv")
+                .coverImage("Samsung.png")
+                .build();
+
+        PageableResponse<CategoryDto> pageableResponse = new PageableResponse<>();
+        pageableResponse.setContent(Arrays.asList(categoryDto1, categoryDto2, categoryDto3));
+
+        Mockito.when(categoryService.getAll(Mockito.anyInt(), Mockito.anyInt(), Mockito.anyString(), Mockito.anyString())).thenReturn(pageableResponse);
+
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders.get("/api/category/")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+
 
     }
 }
