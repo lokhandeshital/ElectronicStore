0b30bc54a66a3edcdb037614ecfb6b8e0eb1a8a1
Shital lokhande <shitallokhande596@gmail.com>
2023-07-16 13:15:26 +0530

    Project Code Changes upto Creating cart Controller for Remove Item from Cart

--- src/main/java/com/bikkadit/electronic/store/controller/CartController.java
+++ src/main/java/com/bikkadit/electronic/store/controller/CartController.java
@@ -1,6 +1,7 @@
 package com.bikkadit.electronic.store.controller;
 
 import com.bikkadit.electronic.store.dtos.AddItemToCartRequest;
+import com.bikkadit.electronic.store.dtos.ApiResponse;
 import com.bikkadit.electronic.store.dtos.CartDto;
 import com.bikkadit.electronic.store.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,18 @@ public class CartController {
 
         CartDto cartDto = cartService.addItemToCart(userId, request);
         return new ResponseEntity<>(cartDto, HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/{userId}/items/{itemId}")
+    public ResponseEntity<ApiResponse> removeItemFromCart(@PathVariable String userId, @PathVariable Integer itemId) {
+
+        cartService.removeItemFromCart(userId, itemId);
+        ApiResponse response = ApiResponse.builder()
+                .message("Item is Removed")
+                .success(true)
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.OK);
 
     }
 
