5b0c788a525e2cc2917453e1a1c34334a2afd0ff
Shital lokhande <shitallokhande596@gmail.com>
2023-07-17 17:17:15 +0530

    Project Code Changes upto Implementing Logger on CartController

--- src/main/java/com/bikkadit/electronic/store/controller/CartController.java
+++ src/main/java/com/bikkadit/electronic/store/controller/CartController.java
@@ -4,6 +4,7 @@ import com.bikkadit.electronic.store.dtos.AddItemToCartRequest;
 import com.bikkadit.electronic.store.dtos.ApiResponse;
 import com.bikkadit.electronic.store.dtos.CartDto;
 import com.bikkadit.electronic.store.service.CartService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/carts/")
+@Slf4j
 public class CartController {
 
     @Autowired
@@ -20,38 +22,44 @@ public class CartController {
     @PostMapping("/{userId}")
     public ResponseEntity<CartDto> addItemToCart(@PathVariable String userId, @RequestBody AddItemToCartRequest request) {
 
+        log.info("Initiated Request for Add Item to Cart with userId : {}", userId);
         CartDto cartDto = cartService.addItemToCart(userId, request);
+        log.info("Completed Request for Add Item to Cart with userId : {}", userId);
         return new ResponseEntity<>(cartDto, HttpStatus.OK);
 
     }
 
     @DeleteMapping("/{userId}/items/{itemId}")
     public ResponseEntity<ApiResponse> removeItemFromCart(@PathVariable String userId, @PathVariable Integer itemId) {
-
+        log.info("Initiated Request for Remove Item From Cart with userId : {}", userId);
         cartService.removeItemFromCart(userId, itemId);
         ApiResponse response = ApiResponse.builder()
                 .message("Item is Removed")
                 .success(true)
                 .build();
+        log.info("Completed Request for Remove Item From Cart with userId : {}", userId);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     //Clear Cart
     @DeleteMapping("/{userId}")
     public ResponseEntity<ApiResponse> clearCart(@PathVariable String userId) {
+        log.info("Initiated Request for Clear Cart with userId : {}", userId);
         cartService.clearCart(userId);
         ApiResponse response = ApiResponse.builder()
                 .message("Cart is Cleared")
                 .success(true)
                 .build();
+        log.info("Completed Request for Clear Cart with userId : {}", userId);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     //getCart
     @GetMapping("/{userId}")
     public ResponseEntity<CartDto> getCart(@PathVariable String userId) {
-
+        log.info("Initiated Request for Get Cart with userId : {}", userId);
         CartDto cartDto = cartService.getCartByUser(userId);
+        log.info("Completed Request for Get Cart with userId : {}", userId);
         return new ResponseEntity<>(cartDto, HttpStatus.OK);
     }
 
